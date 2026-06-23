7ca50d3dd6129ff854b6a2b73a07ce9fd2c2ca21
ISoldatov <isoldatov33@yandex.ru>
2026-06-23 13:15:48 +0300

    feat: add interface Trackable and implemented it in FragileParcel

--- src/ru/yandex/practicum/delivery/FragileParcel.java
+++ src/ru/yandex/practicum/delivery/FragileParcel.java
@@ -2,7 +2,7 @@ package ru.yandex.practicum.delivery;
 
 import static ru.yandex.practicum.delivery.DeliveryApp.FRAGILE_DELIVERY_COST;
 
-public class FragileParcel extends Parcel {
+public class FragileParcel extends Parcel implements Trackable {
     public FragileParcel(String description, int weight, String deliveryAddress, int sendDay) {
         super(description, weight, deliveryAddress, sendDay);
     }
@@ -16,5 +16,10 @@ public class FragileParcel extends Parcel {
     @Override
     public int getDeliveryCost() {
         return FRAGILE_DELIVERY_COST;
+    }
+
+    @Override
+    public void reportStatus(String newLocation) {
+        System.out.println("Хрупкая посылка " + getDescription() + " изменила местоположение на " + newLocation);
     }
 }
